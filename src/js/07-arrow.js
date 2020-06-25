c80e2a97e91918680e766757fee7b87841c2324c
anapy <anabelenfunesc@gmail.com>
2020-06-26 00:18:20 +0200

    adding lesson open button

---
+++ src/js/07-arrow.js
@@ -0,0 +1,12 @@
+'use strict'
+console.log (07);
+
+const click = document.querySelector('.arrow');
+const section = document.querySelector('.colors-container');
+
+function displayArrow() {
+    console.log('click');
+    section.classList.toggle('hidden');
+}
+
+click.addEventListener ('click', displayArrow);
