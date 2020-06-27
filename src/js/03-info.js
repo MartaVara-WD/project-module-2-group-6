a12de2850020dbbdbbf781c98d518a7f9892f809
miriamschaefer <msuarezmelian@gmail.com>
2020-06-27 20:14:57 +0200

    add js name

--- src/js/03-info.js
+++ src/js/03-info.js
@@ -1,1 +1,34 @@
-console.log (03);
+"use strict";
+
+const inputName = document.querySelector(".js-name");
+const inputJob = document.querySelector(".js-job");
+// const inputPhone = document.querySelector(".js-phone");
+// const inputEmail = document.querySelector(".js-email");
+// const inputLinkedin = document.querySelector(".js-linkedin");
+// const inputGithub = document.querySelector(".js-github");
+const printName = document.querySelector(".card__top__title__name");
+
+//empty object
+
+const userData = {};
+
+//función que guarda los datos
+
+function saveFormValues(ev) {
+  userData.name = ev.currentTarget.value;
+  console.log(userData.name);
+}
+
+//envía los datos a la tarjeta
+function sendUserData() {
+  printName.innerHTML = userData.name;
+}
+
+// esta hace magia y lo cambia en la tarjeta
+function printUserData(ev) {
+  saveFormValues(ev);
+  sendUserData();
+}
+
+// inputName.addEventListener("keyup", saveFormValues);
+inputName.addEventListener("keyup", printUserData);
