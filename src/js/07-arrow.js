a4f855eb8be35108b7ad333d0844144965d3f04f
anapy <anabelenfunesc@gmail.com>
2020-06-27 12:50:55 +0200

    cleaning form buttons

--- src/js/07-arrow.js
+++ src/js/07-arrow.js
@@ -1,18 +1,13 @@
 'use strict';
-console.log(07);
-
 const click = document.querySelector('.click');
 const section = document.querySelector('.colors-container');
 const arrowDown = document.querySelector('.arrow-down');
 const arrowUp = document.querySelector('.arrow-up');
 
 function displayArrow() {
-    console.log('click');
-    section.classList.toggle('hidden');
-    arrowUp.classList.toggle('hidden');
-    arrowDown.classList.toggle('hidden');
-
-
+  section.classList.toggle('hidden');
+  arrowUp.classList.toggle('hidden');
+  arrowDown.classList.toggle('hidden');
 }
 
 click.addEventListener ('click', displayArrow);
