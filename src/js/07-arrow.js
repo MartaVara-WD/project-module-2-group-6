53e4b060aab1074d092acdab1a9b7e2780dc498a
anapy <anabelenfunesc@gmail.com>
2020-06-27 18:56:00 +0200

    buttons fill

--- src/js/07-arrow.js
+++ src/js/07-arrow.js
@@ -11,3 +11,17 @@ function displayArrow() {
 }
 
 click.addEventListener ('click', displayArrow);
+
+'use strict';
+const click2 = document.querySelector('.click2');
+const section2 = document.querySelector('.form__section__signup');
+const arrowDown2 = document.querySelector('.arrow-down2');
+const arrowUp2 = document.querySelector('.arrow-up2');
+
+function displayArrow2() {
+  section2.classList.toggle('hidden');
+  arrowUp2.classList.toggle('hidden');
+  arrowDown2.classList.toggle('hidden');
+}
+
+click2.addEventListener ('click', displayArrow2);
