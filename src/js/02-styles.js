0b182df9074bf4082ab4eba64da0110f8399e46c
MarinaCarbonell <marinacarbonellalvarez@gmail.com>
2020-06-29 18:46:38 +0200

    add changes in js

--- src/js/02-styles.js
+++ src/js/02-styles.js
@@ -1,20 +1,37 @@
 'use strict';
-const styles = document.querySelector('.styles');
-const style1 = document.querySelector('.style1');
-const style2 = document.querySelector('.style2');
-const style3 = document.querySelector('.style3');
+const styleBlue = document.querySelector ('.style1');
+const styleRed = document.querySelector ('.style2');
+const styleYellow = document.querySelector ('.style3');
 
-function handlerStyle() {
+//variables del cambio de estilos//
+const colorLeftSquare = document.querySelector ('.js-square');
+const colorName = document.querySelector ('.js-name');
+const colorIcons = document.querySelectorAll ('.js-icon');
+const colorBorder = document.querySelectorAll ('.js-border');
+
+function handlerStyle () {
   //Muestra cuál es el evento actual//
-  console.log(event.currentTarget.classList);
-  if(event.currentTarget.classList.value === 'style1'){
-    console.log(`Ha elegido el estilo 1`);
-  } else if(event.currentTarget.classList.value === 'style2') {
-    console.log(`Ha elegido el estilo 2`);
-  } else if(event.currentTarget.classList.value === 'style3')
-    console.log(`Ha elegido el estilo 3`);
+  console.log (event.currentTarget.classList);
+  if (event.currentTarget.classList.value === 'style1') {
+    console.log ('Ha elegido el estilo 1');
+  } else if (event.currentTarget.classList.value === 'style2') {
+    console.log ('Ha elegido el estilo 2');
+    changeColors ('dark-red', 'medium-red', 'light-red');
+  } else if (event.currentTarget.classList.value === 'style3')
+    console.log ('Ha elegido el estilo 3');
 }
 
-style1.addEventListener ('click', handlerStyle);
-style2.addEventListener ('click', handlerStyle);
-style3.addEventListener ('click', handlerStyle);
+function changeColors (color1, color2, color3) {
+  colorName.classList.add (color1);
+  colorLeftSquare.classList.add (color2);
+  for (const icon of colorIcons) {
+    icon.classList.add (color1);
+  }
+  for (const border of colorBorder) {
+    border.classList.add (color3);
+  }
+}
+
+styleBlue.addEventListener ('click', handlerStyle);
+styleRed.addEventListener ('click', handlerStyle);
+styleYellow.addEventListener ('click', handlerStyle);
