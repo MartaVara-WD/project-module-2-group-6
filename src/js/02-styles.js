4224952bfd0c1ea6925f864d11606d7cdd7d9974
miriamschaefer <msuarezmelian@gmail.com>
2020-06-30 11:01:08 +0200

    fix card js

--- src/js/02-styles.js
+++ src/js/02-styles.js
@@ -2,39 +2,64 @@
 const styleBlue = document.querySelector('.style1');
 const styleRed = document.querySelector('.style2');
 const styleYellow = document.querySelector('.style3');
-
 //variables del cambio de estilos//
 const colorLeftSquare = document.querySelector('.js-square');
-const colorName = document.querySelector('.js-name');
+const colorName = document.querySelector('.js-name-print');
 const colorIcons = document.querySelectorAll('.js-icon');
 const colorBorder = document.querySelectorAll('.js-border');
-
 function handlerStyle() {
   //Muestra cuál es el evento actual//
   console.log(event.currentTarget.classList);
   if (event.currentTarget.classList.value === 'style1') {
     console.log('Ha elegido el estilo 1');
+    changeColors('dark-blue', 'medium-blue', 'light-blue');
+    removeColors('dark-red', 'medium-red', 'light-red');
+    changeColors2('dark-blue-2', 'medium-yellow', 'light-blue-2');
   } else if (event.currentTarget.classList.value === 'style2') {
     console.log('Ha elegido el estilo 2');
     changeColors('dark-red', 'medium-red', 'light-red');
-  } else if (event.currentTarget.classList.value === 'style3');
-  changeColors('dark-blue', 'medium-yellow', 'light-blue');
-  console.log('Ha elegido el estilo 3');
+    removeColors('dark-blue', 'medium-blue', 'light-blue');
+    removeColors2('dark-blue-2', 'medium-yellow', 'light-blue-2');
+  } else if (event.currentTarget.classList.value === 'style3') {
+    console.log('Ha elegido el estilo 2');
+    changeColors('dark-blue-2', 'medium-yellow', 'light-blue-2');
+    removeColors('dark-blue', 'medium-blue', 'light-blue');
+    removeColors2('dark-red', 'medium-red', 'light-red');
+  }
 }
-
 function changeColors(color1, color2, color3) {
   colorName.classList.add(color1);
-
   colorLeftSquare.classList.add(color2);
+
   for (const icon of colorIcons) {
     icon.classList.add(color1);
-    icon.classList.remove(color2);
-    icon.classList.remove(color3);
   }
   for (const border of colorBorder) {
     border.classList.add(color3);
-    border.classList.remove(color2);
-    border.classList.remove(color1);
+  }
+}
+
+function removeColors(color1, color2, color3) {
+  colorName.classList.remove(color1);
+  colorLeftSquare.classList.remove(color2);
+
+  for (const icon of colorIcons) {
+    icon.classList.remove(color1);
+  }
+  for (const border of colorBorder) {
+    border.classList.remove(color3);
+  }
+}
+
+function removeColors2(color1, color2, color3) {
+  colorName.classList.remove(color1);
+  colorLeftSquare.classList.remove(color2);
+
+  for (const icon of colorIcons) {
+    icon.classList.remove(color1);
+  }
+  for (const border of colorBorder) {
+    border.classList.remove(color3);
   }
 }
 
