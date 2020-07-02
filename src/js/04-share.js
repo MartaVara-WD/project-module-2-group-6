a9d78f2e76775afd468c12d2db01b98d869ccf6b
anapy <anabelenfunesc@gmail.com>
2020-07-02 19:42:54 +0200

    fixing share button

--- src/js/04-share.js
+++ src/js/04-share.js
@@ -2,11 +2,12 @@
 
 const buttonCard = document.querySelector ('.js-share');
 const buttonTwitter = document.querySelector ('.js-twitter');
+const buttonContainer = document.querySelector('.form__share__button');
 
 function createCard (ev) {
   ev.preventDefault ();
-  buttonCard.classList.remove ('active');
-  buttonCard.classList.add ('inactive');
+  buttonContainer.classList.remove ('active');
+  buttonContainer.classList.add ('inactive');
   buttonTwitter.classList.remove ('hidden');
 }
 
