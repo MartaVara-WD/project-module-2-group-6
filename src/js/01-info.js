9d2edd6307d432e008302bc7fd81796864410563
miriamschaefer <msuarezmelian@gmail.com>
2020-07-14 18:35:52 +0100

    fix card url link

--- src/js/01-info.js
+++ src/js/01-info.js
@@ -214,6 +214,7 @@ function storageData() {
       sendUserData(item);
     }
   }
+  buttonActive();
 }
 
 function tickPalette() {
--- src/js/06-share-button.js
+++ src/js/06-share-button.js
@@ -1,18 +1,22 @@
 'use strict';
 
 function buttonActive() {
+  let inputValueName = input.name.value;
+  let inputValueJob = input.job.value;
   let inputValueEmail = input.email.value;
   let inputValueGithub = input.github.value;
   let inputValuePhone = input.phone.value;
   let inputValueLinkedin = input.linkedin.value;
 
   if (
-    inputValueEmail !== 0 &&
-    inputValueGithub !== 0 &&
-    inputValuePhone !== 0 &&
-    inputValueLinkedin !== 0
+    inputValueName.length !== 0 &&
+    inputValueJob.length !== 0 &&
+    inputValueEmail.length !== 0 &&
+    inputValueGithub.length !== 0 &&
+    inputValuePhone.length !== 0 &&
+    inputValueLinkedin.length !== 0
   ) {
-    buttonCard.classList.add('active');
-    buttonCard.classList.remove('inactive');
+    buttonCard.classList.toggle('active');
+    buttonCard.classList.toggle('inactive');
   }
 }
